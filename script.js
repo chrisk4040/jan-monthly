a4a3d9ebbb7110bc48b5113ce046aaa77c4082c1
chrisk4040 <526ckozeniesky@frhsd.com>
2025-01-17 09:28:48 -0500

    finished everything just need to conent element by id

--- script.js
+++ script.js
@@ -1,74 +1,83 @@
-let computerScore= 0;
+let computerScore = 0;
 let userScore = 0;
 
 const getUserChoice = userInput => {
     userInput = userInput.toLowerCase();
-    }
-
+    return userInput;
+}
 
 const getComputerChoice = () => {
     const randomNumber = Math.floor(Math.random() * 3);
     switch (randomNumber) {
-      case 0:
-        return 'rock';
-      case 1:
-        return 'paper';
-      case 2:
-        return 'scissors';
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
     }
-  };
-  
-  const determineWinner = (userChoice, computerChoice) => {
+};
+
+const determineWinner = (userChoice, computerChoice) => {
     if (userChoice === computerChoice) {
         return 'The game is a tie!';
     }
     if (userChoice === 'rock') {
         if (computerChoice === 'paper') {
-          return 'Computer won.';
-          } else {
+            computerScore++;
+            return 'Computer won.';
+        } else {
+            userScore++;
             return 'You won!';
-          }
+        }
     }
-  
+
     if (userChoice === 'paper') {
         if (computerChoice === 'scissors') {
+            computerScore++;
             return 'Computer won.';
         } else {
+            userScore++;
             return 'You won!';
         }
     }
-  
+
     if (userChoice === 'scissors') {
         if (computerChoice === 'rock') {
+            computerScore++;
             return 'Computer won.';
         } else {
+            userScore++;
             return 'You won!';
         }
     }
-  
+
     if (userChoice === 'bomb') {
+        userScore++;
         return 'You won!';
     }
-  };
-  
-    const playGame = (userChoice) => {
-      const computerChoice = getComputerChoice();
-      console.log(`You chose: ${userChoice}.`);
-      console.log(`Computer chose: ${computerChoice}.`);
-     
-      console.log(determineWinner(userChoice, computerChoice))
-  };
-  
+};
 
-  if computerWon(){
-    return `1 + ${computerScore}`
-  }; else if userWon(){
-    return `1 + ${userScore}`
-  }
+const playGame = (userChoice) => {
+    const computerChoice = getComputerChoice();
+    console.log(`You chose: ${userChoice}.`);
+    console.log(`Computer chose: ${computerChoice}.`);
 
+    const result = determineWinner(userChoice, computerChoice);
+    console.log(result);
+    console.log(`User Score: ${userScore}, Computer Score: ${computerScore}`);
 
+    if (userScore === 3) {
+        console.log('You are the overall winner!');
+        resetGame();
+    } else if (computerScore === 3) {
+        console.log('Computer is the overall winner!');
+        resetGame();
+    }
+};
 
-switch (computerScore){
-    case 0:
-    return 
-}
+const resetGame = () => {
+    computerScore = 0;
+    userScore = 0;
+    console.log('The game has been reset.');
+};
